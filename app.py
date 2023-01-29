b459fa4299afb192a058bb061c9eb5a215dfffe9
abdelrahman2512 <105170957+abdelrahman2512@users.noreply.github.com>
2023-01-29 13:17:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-import flask ,requests
-from flask import * 
+import mindai
+
+from flask import *
+
+import random
+
+ai = mindai.Ai()
 
 app = Flask(__name__)
 
-@app.route("/imge")
-def img():
-	text = request.args.get("text")
-	if not text:
-		return {"status":"error"}
-	url = f"https://api.dlyar-dev.tk/open-ai-img.json?key=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&text={text}"
-	r = requests.get(url).json()
-	i = r["data"][0]["url"]
-	return {"status":"done",
-	"url":i}
+@app.route("/get")
+
+def get():
+
+		text = request.args.get("text")
+
 	
+
+	imgs = ai.image_search(text)
+
+	
+
+	return {"status":"done","url":random.choice(imgs["images"])}
+
+	
+
 if __name__ =="__main__":
-	app.run(debug=True,host="0.0.0.0",port=81)
+
+	app.run(debug=True,port=81,host="0.0.0.0")
